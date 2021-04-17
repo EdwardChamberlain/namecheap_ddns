6179bd5545a97944ef307e8fbca76fc0e1daaca8
Edward Chamberlain <edward.chamberlain@me.com>
2021-04-17 23:37:27 +0100

    initial commit

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from requests import get
+import time
+import datetime
+import os
+
+env = ['APP_DOMAIN', 'APP_HOST', 'APP_PASSWORD',]
+
+for e in env:
+    assert e in os.environ, f'You must provide an "{e}" variable'
+
+domain = os.environ['APP_DOMAIN']
+host = os.environ['APP_HOST']
+password = os.environ['APP_PASSWORD']
+
+
+ip = None
+while True:
+    prev_ip = ip
+    ip = get('https://api.ipify.org').text
+
+    if ip != prev_ip:
+        res = get(f'https://dynamicdns.park-your-domain.com/update?host={host}&domain={domain}&password={password}&ip={ip}')
+
+        print(f'{datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")}: NEW public IP address: {ip} - UPDATE request sent.')
+
+    time.sleep(3)
