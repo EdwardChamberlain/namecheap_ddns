0bb5cf3d6c8e1336ecf07fa8290d5cefb12cca16
Edward Chamberlain <edward.chamberlain@me.com>
2021-04-20 08:19:56 +0100

    broke update out to a function.

--- src/main.py
+++ src/main.py
@@ -7,6 +7,16 @@ import logging
 import config
 
 
+def update_ip(host, domain, password):
+    res = get(f"https://dynamicdns.park-your-domain.com/update?host={host}&domain={domain}&password={password}")
+    error = re.search(r'(?:(?:<ResponseString>)(.+)(?:<\/ResponseString>))', res.text)
+
+    if error:
+        logging.error(f"an error occured: <{error[1]}>")
+    else:
+        logging.info(f"IP updated successfully")
+
+
 logging.info("Starting Script")
 
 missing_vars = config.required_vars - set(os.environ.keys())
@@ -16,12 +26,9 @@ if missing_vars:
 
 
 while True:
-    res = get(f"https://dynamicdns.park-your-domain.com/update?host={os.environ['APP_HOST']}&domain={os.environ['APP_DOMAIN']}&password={os.environ['APP_PASSWORD']}")
-    error = re.search(r'(?:(?:<ResponseString>)(.+)(?:<\/ResponseString>))', res.text)
-
-    if error:
-        logging.error(f"an error occured: <{error[1]}>")
-    else:
-        logging.info(f"IP updated successfully")
-        
+    update_ip(
+        host=os.environ['APP_HOST'],
+        domain=os.environ['APP_DOMAIN'],
+        password=os.environ['APP_PASSWORD'],
+    )  
     time.sleep(float(os.getenv('APP_UPDATE_TIME') or 60))
